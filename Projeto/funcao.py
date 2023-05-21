c69c0ca15bc6698c20c96ac1d7dd46d48253ef2d
igoralvesa <101803586+igoralvesa@users.noreply.github.com>
2023-05-21 20:15:50 -0300

    Add files via upload

---
+++ Projeto/funcao.py
@@ -0,0 +1,25 @@
+def menu():
+    print("-"*76)
+    print(" \t \t \t 1- Adicionar \t \t ")
+    print(" \t \t \t 2- Ler \t \t ")
+    print(" \t \t \t 3- Atualizar \t \t ")
+    print(" \t \t \t 4- Deletar \t \t\t ")
+    print(" \t \t \t 5- Sair \t \t\t ")
+    print("-"*76) 
+    
+def tabela_categoria():   
+    print("CATEGORIAS")
+    print("\t \t \t 1 - CASA \t \t \t")
+    print("\t \t \t 2 - COMIDA \t \t \t")
+    print("\t \t \t 3 - TRANSPORTE \t \t \t")
+    print("\t \t \t 4 - USO PESSOAL \t \t \t")
+    print("-"*76)
+    
+def tabela_categoria_ler():   
+    print("CATEGORIAS")
+    print("\t \t \t 1 - CASA \t \t \t")
+    print("\t \t \t 2 - COMIDA \t \t \t")
+    print("\t \t \t 3 - TRANSPORTE \t \t \t")
+    print("\t \t \t 4 - USO PESSOAL \t \t \t")
+    print("\t \t \t 5 - GASTOS TOTAIS \t \t \t")
+    print("-"*76)
