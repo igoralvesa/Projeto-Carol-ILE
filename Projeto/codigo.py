6e62efcf90cfe0dde79ca35f65250e2cd718772f
luizlins <luizopluiz@outlook.com>
2023-05-21 22:13:53 -0300

    teste commit!

--- Projeto/codigo.py
+++ Projeto/codigo.py
@@ -5,19 +5,22 @@ from funcao import tabela_categoria_ler
 dic = {1: "CASA", 2: "COMIDA", 3: "TRANSPORTE", 4: "USOPESSOAL"}
 decisao = int(input("voce deseja entrar no menu? [ 1- SIM, 2- NÃO ] "))
 
+
 def adicionar():
     arquivo = open("dados.csv", "a")
-    nome = input("Com o que você gastou? :: ").capitalize()
+    nome = input("Com o que você gastou? ::: ").capitalize()
     valor = float(input("Quanto custou? "))
     tabela_categoria()
     escolha_categoria = int(input("O gasto se encaixa em qual categoria? "))
     arquivo.write(f"{nome}\t{valor}R$ ----- {dic[escolha_categoria]}\n")
     arquivo.close()
 
+
 def ler():
-    
+
     tabela_categoria_ler()
-    escolha_ler = int(input("Digite a categoria que você deseja ver os gastos: "))
+    escolha_ler = int(
+        input("Digite a categoria que você deseja ver os gastos: "))
     if escolha_ler == 5:
         print("Gastos totais:\n")
         for i in range(1, 5):
@@ -35,48 +38,50 @@ def ler():
         arquivo.close()
     print()
 
+
 def atualizar():
     return 0
 
-def deletar():  
+
+def deletar():
     with open("dados.csv", 'r') as f:
         linhas = f.readlines()
     f.close()
-    
+
     arquivo = open("dados.csv", "r")
     for i in arquivo:
         exc = i.find(next(filter(str.isdigit, i)), 0)
         print(f"{i[:exc]}")
     arquivo.close()
-    
+
     excluir = input("Qual transação você deseja deletar? ").capitalize()
-    
+
     arquivo = open("dados.csv", "w")
     for linha in linhas:
         if excluir not in linha:
-            arquivo.write(linha)               
+            arquivo.write(linha)
     arquivo.close()
-    
+
 
 while decisao == 1:
     menu()
-    
+
     pergunta = int(input("o que voce deseja realizar? "))
-    
+
     if pergunta == 1:
         adicionar()
-   
+
     elif pergunta == 2:
         ler()
-    
+
     elif pergunta == 3:
         atualizar()
-    
+
     elif pergunta == 4:
         deletar()
-    
-    #Sair
+
+    # Sair
     else:
         break
-        
+
     print("retornando ao menu.....")
