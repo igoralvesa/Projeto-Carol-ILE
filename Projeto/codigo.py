ea5e94c92d5027e4ed208d5e04c08f02c967fb4b
luizlins <132574500+luizlins@users.noreply.github.com>
2023-05-24 12:35:09 -0300

    Atualização 3 (TRY EXCEPT).py

--- Projeto/codigo.py
+++ Projeto/codigo.py
@@ -4,7 +4,17 @@ from funcao import tabela_categoria
 from funcao import tabela_categoria_ler
 
 dic = {1: "CASA", 2: "COMIDA", 3: "TRANSPORTE", 4: "USOPESSOAL"}
-decisao = int(input("voce deseja entrar no menu? [ 1- SIM, 2- NÃO ] "))
+while True:
+    try:
+        valores = [1, 2]
+        decisao = int(input("você deseja entrar no menu? [1 - SIM, 2 - NÃO] "))
+        if decisao in valores:
+            break
+        else:
+            print("VALOR INVÁLIDO! as opções são 1 ou 2")
+    except ValueError:
+        print("LETRAS NÃO SÃO ACEITAS! informe um valor válido")
+ 
 
 
 def adicionar():
@@ -14,15 +24,23 @@ def adicionar():
         try:
          valor = float(input("Quanto custou? "))
          break
+        except KeyError:
+            print("informe um valor válido.")
         except ValueError:
             print("informe um valor válido")
     tabela_categoria()
+    
     while True:
         try:
+         valores = [1, 2, 3, 4]
          escolha_categoria = int(input("O gasto se encaixa em qual categoria? "))
-         break
+         if escolha_categoria in valores:
+             break
+         else:
+             print("VALOR INVÁLIDO! as opções são de 1 a 4")
+         
         except ValueError:
-            print("insira um valor válido")
+            print("LETRAS NÃO SÃO ACEITAS! informe um valor válido")
     arquivo.write(f"{nome}\t{valor}R$ ----- {dic[escolha_categoria]}\n")
     arquivo.close()
 
@@ -32,10 +50,14 @@ def ler():
     tabela_categoria_ler()
     while True:
         try:
+         valores = [1,2,3,4,5]
          escolha_ler = int(input("Digite a categoria que você deseja ver os gastos: "))
-         break
+         if escolha_ler in valores:
+             break
+         else:
+             print("VALOR INVÁLIDO! as opções são de 1 a 5")
         except ValueError:
-            print("insira um valor válido")
+            print("LETRAS NÃO SÃO ACEITAS! informe um valor válido")
     if escolha_ler == 5:
         print("Gastos totais:\n")
         for i in range(1, 5):
@@ -83,18 +105,25 @@ def deletar():
             print("DELETADO!")
             arquivo.close()
             break
-        
+
         else:
-            print("Digite uma transação válida: ", "\n")
+            print("Digite uma transação válida! ", "\n")
 
 
 while decisao == 1:
     menu()
     while True:
         try:
+            valores = [1,2,3,4,5]
             pergunta = int(input("O que você deseja realizar? "))
             os.system('cls')
-            break
+            if pergunta in valores:
+                break
+            else:
+                print("VALOR INVÁLIDO! as opções são de 1 a 5")
+                menu()
+        except KeyError:
+            print("informe um valor válido.")
         except ValueError:
             print("Informe um valor válido.")
     if pergunta == 1:
@@ -109,7 +138,7 @@ while decisao == 1:
     elif pergunta == 4:
         deletar()
 
-    # Sair
+    
     else:
         break
 
